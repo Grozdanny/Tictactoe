b5324c5742381124abe5d1497cc3022ffd73f24c
Grozdanny <marcinzur277@gmail.com>
2021-10-11 09:04:44 +0000

    Fixed places for files ba

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom"
-
+import ReactDOM from "react-dom";
+import "./index.css";
 class Square extends React.Component {
   constructor(props) {
     super(props);
